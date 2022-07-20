a9593e8d8f6d26489b7c6050b5817cba18f25b36
nguyentrungquang2001 <quangnt@aisolutionsjsc.com>
2022-07-20 22:13:20 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { FaMapMarkerAlt } from "react-icons/fa";
+import data from "./data";
 function App() {
+  const content = data.map((item) => {
+    return (
+      <div className="content">
+        <div className="contentAvatar">
+          <img src={item.imageUrl} />
+        </div>
+        <div className="contentText">
+          <div className="add">
+            <div className="addLive">
+              <FaMapMarkerAlt color="red" />
+              {item.location}
+            </div>
+            <div className="addMap">
+              <a href={"http://google.com"}>View on Google Maps</a>
+            </div>
+          </div>
+          <div className="name"> {item.title}</div>
+          <div className="day">
+            {item.startDate} - {item.endDate}
+          </div>
+          <div className="des">{item.description}</div>
+        </div>
+        <div className="borderBottom"></div>
+      </div>
+    );
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <header>
+        <p>my travel journal</p>
       </header>
+      <div>{content}</div>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,34 @@
+export default [
+  {
+    title: "Mount Fuji",
+    location: "Japan",
+    googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+    startDate: "12 Jan, 2021",
+    endDate: "24 Jan, 2021",
+    description:
+      "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+    imageUrl: "https://source.unsplash.com/WLxQvbMyfas",
+  },
+  {
+    title: "Sydney Opera House",
+    location: "NORWAY",
+    googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+    startDate: "12 Jan, 2021",
+    endDate: "24 Jan, 2021",
+    description:
+      "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+    imageUrl:
+      "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/17/3b/95/61/photo2jpg.jpg?w=1200&h=-1&s=1",
+  },
+  {
+    title: "Geirangerfjord",
+    location: "AUSTRALIA",
+    googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+    startDate: "12 Jan, 2021",
+    endDate: "24 Jan, 2021",
+    description:
+      "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/7/7f/207_Geirangerfjord.jpg",
+  },
+];
